d2ee346789deea8efa858e78aa1f4940b757e1dd
Videlefsky <j.videlefsky@northeastern.edu>
2023-04-12 22:25:43 -0400

    Test Case start

---
+++ 13_Test_Cases.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------------------------------------------------------------------------
+-- TESTS FOR DB_ADMIN
+-- Tables
+
+
+
+-- TESTS FOR DEVELOPER_MANAGER
+-- Tables
+select * from db_admin.user_info;
+-- Insert
+
+-- Procedures
+
+
+-- TESTS FOR USER_MANAGER
+-- Tables
+select * from db_admin.application;
+-- Insert
+
+-- Procedures
